52d70c0a53882f8feddf4c9fd1320aff6d0152b5
Bill Dueber <bill@dueber.com>
2014-11-19 15:34:13 -0500

    Get simple queries working

--- lib/simple_solr/core/search.rb
+++ lib/simple_solr/core/search.rb
@@ -1,14 +1,20 @@
+require 'simple_solr/response/query_response'
+
 module SimpleSolr::Core::Search
 
   def fv_search(field, value)
     v = value
     v = SimpleSolr.lucene_escape Array(value).join(' ') unless v == '*'
     kv = "#{field}:#{v}"
-    get('select', {:q => kv})
+    get('select', {:q => kv}, SimpleSolr::Response::QueryResponse)
   end
 
   def all
     fv_search('*', '*')
+  end
+
+  def id(i)
+    fv_search('id', i).first
   end
 
 
